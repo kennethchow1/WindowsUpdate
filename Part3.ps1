141de874caf2789131ad85e8707f657adb181bfa
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-06-19 21:31:16 -0700

    Add files via upload

---
+++ Part3.ps1
@@ -0,0 +1,3 @@
+﻿echo "Initializing Final Updates.."
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/kennethchow1/WindowsUpdate/main/Part4.ps1" -OutFile "$env:HOMEPATH\AppData\Local\Temp\Part4.ps1"
+pwsh "$env:HOMEPATH\AppData\Local\Temp\Part4.ps1"
---
+++ Part4.ps1
@@ -0,0 +1,8 @@
+﻿echo "Starting Final Stage"
+Install-Module PSWindowsUpdate -Confirm:$false -force
+Add-WUServiceManager -MicrosoftUpdate -Confirm:$false
+Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot
+Powercfg /batteryreport
+Start msedge –no-first-run ./battery-report.html
+Start msedge –no-first-run https://retest.us/laptop-no-keypad
+Start calc
