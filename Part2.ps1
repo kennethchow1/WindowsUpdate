d3f3ed6ef30c0ed7239451d8f7512257429fa85d
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-06-19 21:11:28 -0700

    Update Part2.ps1

--- Part2.ps1
+++ Part2.ps1
@@ -1,5 +1,6 @@
 Install-Module PSWindowsUpdate -Confirm:$false -force
 Add-WUServiceManager -MicrosoftUpdate -Confirm:$false
+echo "Starting Updates"
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
