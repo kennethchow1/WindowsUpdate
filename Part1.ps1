3fb00c2ea94a6553f39652a683ebc3b56f490575
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-06-19 20:08:13 -0700

    Update Part1.ps1

--- Part1.ps1
+++ Part1.ps1
@@ -1,4 +1,4 @@
-﻿wget https://github.com/PowerShell/PowerShell/releases/download/v7.4.2/PowerShell-7.4.2-win-x64.msi -OutFile Pwsh.msi
-msiexec.exe /package Pwsh.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
+Invoke-WebRequest -Uri "https://github.com/PowerShell/PowerShell/releases/download/v7.4.2/PowerShell-7.4.2-win-x64.msi" -OutFile "C:\Pwsh.msi"
+msiexec.exe /package "C:\Pwsh.msi" /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
 Start-Sleep -Seconds 15
-pwsh C:\Users\vboxuser\Desktop\Part2.ps1
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
