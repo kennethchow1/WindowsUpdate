ddf7d8d12bf7d556a29496642dea506553369716
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-06-20 21:19:40 -0700

    Update Part3.ps1

--- Part3.ps1
+++ Part3.ps1
@@ -1,2 +1,2 @@
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/kennethchow1/WindowsUpdate/main/Part4.ps1" -OutFile "$env:HOMEPATH\AppData\Local\Temp\Part4.ps1"
-Start pwsh "$env:HOMEPATH\AppData\Local\Temp\Part4.ps1"
+pwsh "$env:HOMEPATH\AppData\Local\Temp\Part4.ps1"
