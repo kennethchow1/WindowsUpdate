66d696886b9ddedb7818057402992812f46af1b6
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-06-21 18:45:29 -0700

    Update Part1.ps1

--- Part1.ps1
+++ Part1.ps1
@@ -1,6 +1,6 @@
 echo "Initializing.."
-Invoke-WebRequest -Uri "https://github.com/PowerShell/PowerShell/releases/download/v7.4.2/PowerShell-7.4.2-win-x64.msi" -OutFile "$env:HOMEPATH\AppData\Local\Temp\Pwsh.msi"
+Invoke-WebRequest -Uri "https://github.com/PowerShell/PowerShell/releases/download/v7.4.2/PowerShell-7.4.2-win-x64.msi" -OutFile Pwsh.msi
 echo "Powershell 7 Installing, Please Wait."
-msiexec.exe /package "$env:HOMEPATH\AppData\Local\Temp\Pwsh.msi" /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
+msiexec.exe /package Pwsh.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
 Start-Sleep -Seconds 25
 Start-Process -FilePath "C:\Program Files\Powershell\7\pwsh.exe" -ArgumentList "-ExecutionPolicy Unrestricted -C irm https://raw.githubusercontent.com/kennethchow1/WindowsUpdate/main/Part2.ps1 | iex"
