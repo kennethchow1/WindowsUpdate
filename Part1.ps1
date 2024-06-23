fccd1d785cc3b940d9913ad03edede8710d29f25
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-06-23 10:20:56 -0700

    Update Part1.ps1

--- Part1.ps1
+++ Part1.ps1
@@ -4,4 +4,4 @@ echo "Powershell 7 Installing, Please Wait."
 msiexec.exe /package Pwsh.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
 Start-Sleep -Seconds 25
 Invoke-WebRequest -Uri "https://kennethchow1.github.io/WindowsUpdate/Final.lnk" -OutFile "$env:HOMEPATH\Desktop\Finalize.lnk"
-Start-Process -FilePath "C:\Program Files\Powershell\7\pwsh.exe" -ArgumentList "-ExecutionPolicy Unrestricted -C irm https://kennethchow1.github.io/WindowsUpdate/Part2.ps1 | iex"
+Start-Process -FilePath "C:\Program Files\Powershell\7\pwsh.exe" -ArgumentList "-ExecutionPolicy Unrestricted -C irm https://getupdates.me/Part2.ps1 | iex"
