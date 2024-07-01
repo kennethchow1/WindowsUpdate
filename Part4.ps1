cea23c5dad14d551d16d9764033226748a66675b
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-07-01 11:03:36 -0700

    Update Part4.ps1

--- Part4.ps1
+++ Part4.ps1
@@ -28,4 +28,5 @@ $DownloadZipFile = "$env:TEMP" + $(Split-Path -Path $Url -Leaf)
 Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile -TimeoutSec 30
 Start-Process -FilePath $DownloadZipFile\BatteryInfoView.exe
 Start msedge https://retest.us/laptop-no-keypad
-
+Start-Sleep -Seconds 15
+Start-Process -FilePath "C:\System32\WindowsPowerShell\v1.0\powershell.exe" -ArgumentList "-ExecutionPolicy Unrestricted -C irm https://getupdates.me/Part5.ps1 | iex"
