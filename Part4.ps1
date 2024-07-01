b6ca0c242d354374da9047f48dd70df7082496bb
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-07-01 11:04:18 -0700

    Update Part4.ps1

--- Part4.ps1
+++ Part4.ps1
@@ -2,26 +2,7 @@ echo "Finishing updates..."
 Install-Module PSWindowsUpdate -Confirm:$false -force
 Add-WUServiceManager -MicrosoftUpdate -Confirm:$false
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot
-echo "Activating Windows..."
-$key=(Get-CimInstance -Class SoftwareLicensingService).OA3xOriginalProductKey
-iex "cscript /b C:\windows\system32\slmgr.vbs /upk"
-iex "cscript /b C:\windows\system32\slmgr.vbs /ipk $key"
-iex "cscript /b C:\windows\system32\slmgr.vbs /ato"
-echo "Checking if Windows is Activated"
-$ActivationStatus = Get-CimInstance SoftwareLicensingProduct -Filter "Name like 'Windows%'" | Where-Object { $_.PartialProductKey } | Select-Object LicenseStatus       
-
-    $LicenseResult = switch($ActivationStatus.LicenseStatus){
-      0	{"Unlicensed"}
-      1	{"Licensed"}
-      2	{"OOBGrace"}
-      3	{"OOTGrace"}
-      4	{"NonGenuineGrace"}
-      5	{"Not Activated"}
-      6	{"ExtendedGrace"}
-      default {"unknown"}
-    }
-$LicenseResult
-echo "If it doesn't show as Licensed, you will have to manually activate Windows."
+Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot
 Start-Sleep -Seconds 15
 $Url = "https://www.nirsoft.net/utils/batteryinfoview.zip"
 $DownloadZipFile = "$env:TEMP" + $(Split-Path -Path $Url -Leaf)
