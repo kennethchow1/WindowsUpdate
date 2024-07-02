2ab5ce5f676ed2b1c10b7a2de7c55abac3cff7e2
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-07-02 10:33:55 -0700

    Update Part4.ps1

--- Part4.ps1
+++ Part4.ps1
@@ -4,9 +4,34 @@ Add-WUServiceManager -MicrosoftUpdate -Confirm:$false
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot
 Start-Sleep -Seconds 15
+echo "Activating Windows..."
+$key=(Get-WmiObject -Class SoftwareLicensingService).OA3xOriginalProductKey
+iex "cscript /b C:\windows\system32\slmgr.vbs /upk"
+iex "cscript /b C:\windows\system32\slmgr.vbs /ipk $key"
+iex "cscript /b C:\windows\system32\slmgr.vbs /ato"
+echo "Checking if Windows is Activated... If it doesn't show as Licensed, you will have to manually activate Windows."
+$ActivationStatus = Get-CimInstance SoftwareLicensingProduct -Filter "Name like 'Windows%'" | Where-Object { $_.PartialProductKey } | Select-Object LicenseStatus       
+
+    $LicenseResult = switch($ActivationStatus.LicenseStatus){
+      0	{"Unlicensed"}
+      1	{"Licensed"}
+      2	{"OOBGrace"}
+      3	{"OOTGrace"}
+      4	{"NonGenuineGrace"}
+      5	{"Not Activated"}
+      6	{"ExtendedGrace"}
+      default {"unknown"}
+    }
+$LicenseResult
 $Url = "https://www.nirsoft.net/utils/batteryinfoview.zip"
 $DownloadZipFile = "$env:TEMP" + $(Split-Path -Path $Url -Leaf)
 Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile -TimeoutSec 30
 Start-Process -FilePath $DownloadZipFile\BatteryInfoView.exe
 Start msedge https://retest.us/laptop-no-keypad
-Start-Sleep -Seconds 15
+# If running in the console, wait for input before closing.
+if ($Host.Name -eq "ConsoleHost")
+{
+    Write-Host "Press any key to exit."
+    $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp") > $null
+}
+
