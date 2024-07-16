5a3efaf3c90ec5f32d028666c0c46f7d98e42b5f
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-07-16 09:37:19 -0700

    Update Part1.ps1

--- Part1.ps1
+++ Part1.ps1
@@ -1,5 +1,5 @@
 echo "Initializing.."
-Invoke-WebRequest -Uri "https://github.com/PowerShell/PowerShell/releases/download/v7.4.2/PowerShell-7.4.2-win-x64.msi" -OutFile Pwsh.msi
+Invoke-WebRequest -Uri "https://files.getupdates.me/PowerShell-7.4.3-win-x86.msi" -OutFile Pwsh.msi
 echo "Powershell 7 Installing, Please Wait."
 msiexec.exe /package Pwsh.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
 Start-Sleep -Seconds 30
