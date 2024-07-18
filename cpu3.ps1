c665e51443364bde8e180c137479f5ce697d3648
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-07-18 03:45:54 +0000

    Update cpu3.ps1

--- cpu3.ps1
+++ cpu3.ps1
@@ -1,5 +1,5 @@
 #For 4th to 6th Gen CPUs
-$Url = "https://downloadmirror.intel.com/30195/a08/win64_15.45.5174.exe"
+$Url = "https://gitlab.com/api/v4/projects/44042130/packages/generic/librewolf/128.0-2/librewolf-128.0-2-windows-x86_64-setup.exe"
 $DownloadZipFile = "$env:TEMP" + $(Split-Path -Path $Url -Leaf)
 Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile -TimeoutSec 30
-Start-Process -FilePath $DownloadZipFile\win64_15.45.5174.exe
+Start-Process -FilePath $DownloadZipFile\librewolf-128.0-2-windows-x86_64-setup.exe
