eda68af29befc3c51f795df80aea201c7d7a8fcf
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-07-18 03:18:41 +0000

    Update gpu1.ps1

--- gpu1.ps1
+++ gpu1.ps1
@@ -1,3 +1,4 @@
+#For Intel 11th Gen and newer CPUs
 $Url = "https://downloadmirror.intel.com/827043/gfx_win_101.5762.exe"
 $DownloadZipFile = "$env:TEMP" + $(Split-Path -Path $Url -Leaf)
 Invoke-WebRequest -Uri "https://downloadmirror.intel.com/827043/gfx_win_101.5762.exe" -OutFile $DownloadZipFile -TimeoutSec 30
