f351e4343bf4c77d0f3f610784565234f79a0264
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-07-18 10:25:14 -0700

    Update test3.ps1

--- test3.ps1
+++ test3.ps1
@@ -27,11 +27,13 @@ $ActivationStatus = Get-CimInstance SoftwareLicensingProduct -Filter "Name like 
     }
 $LicenseResult
 echo "If you are missing Intel Integrated GPU Drivers, please use the corresponding desktop shortcut for your CPU Generation."
+Unregister-ScheduledTask -TaskName 'Final' -Confirm:$false
 $Url = "https://www.nirsoft.net/utils/batteryinfoview.zip"
 $DownloadZipFile = "$env:TEMP" + $(Split-Path -Path $Url -Leaf)
 Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile -TimeoutSec 30
 Start-Process -FilePath $DownloadZipFile\BatteryInfoView.exe
 Start msedge https://retest.us/laptop-no-keypad, https://testmyscreen.com, https://getupdates.me/drivers
+Unregister-ScheduledTask -TaskName 'Final' -Confirm:$false
 # If running in the console, wait for input before closing.
 if ($Host.Name -eq "ConsoleHost")
 {
