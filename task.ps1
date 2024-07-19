92a52732b09a659a56dfb4e18f7a8765905bf3aa
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-07-19 14:47:33 -0700

    Update task.ps1

--- task.ps1
+++ task.ps1
@@ -5,7 +5,7 @@ $action = New-ScheduledTaskAction -Execute 'pwsh.exe' -Argument '-Command "irm h
 $principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 
 ## set to run at startup could also do -AtLogOn for the trigger
-$trigger = New-ScheduledTaskTrigger -AtStartup
+$trigger = New-ScheduledTaskTrigger -AtLogOn
 
 ## register it (save it) and it will show up in default folder of task scheduler.
 Register-ScheduledTask -Action $action -TaskName 'mytask' -TaskPath '\' -Principal $principal -Trigger $trigger
