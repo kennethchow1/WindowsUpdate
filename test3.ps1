e397db5455f621076dacc99e0c4bb7eae9e41f17
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-07-19 11:06:06 -0700

    Update test3.ps1

--- test3.ps1
+++ test3.ps1
@@ -3,7 +3,7 @@ Invoke-WebRequest -Uri "https://getupdates.me/Intel_11th_Gen_Drivers.lnk" -OutFi
 Invoke-WebRequest -Uri "https://getupdates.me/Intel_7th-10th_Gen_Drivers.lnk" -OutFile "$env:HOMEPATH\Desktop\Intel 7th-10th Gen Drivers.lnk"
 Invoke-WebRequest -Uri "https://getupdates.me/Intel_4th-6th_Gen_Drivers.lnk" -OutFile "$env:HOMEPATH\Desktop\Intel 4th-6th Gen Drivers.lnk"
 Invoke-WebRequest -Uri "https://github.com/ungoogled-software/ungoogled-chromium-windows/releases/download/126.0.6478.126-1.1/ungoogled-chromium_126.0.6478.126-1.1_windows_x64.zip" -OutFile "$env:TEMP\ungoogled-chromium_126.0.6478.126-1.1_windows_x64.zip"
-Expand-Archive -LiteralPath "$env:TEMP\ungoogled-chromium_126.0.6478.126-1.1_windows_x64\ungoogled-chromium_126.0.6478.126-1.1_windows\chrome.exe" -DestinationPath "$env:TEMP\"
+Expand-Archive -LiteralPath "$env:TEMP\ungoogled-chromium_126.0.6478.126-1.1_windows_x64.zip" -DestinationPath "$env:TEMP\"
 Install-Module PSWindowsUpdate -Confirm:$false -force
 Add-WUServiceManager -MicrosoftUpdate -Confirm:$false
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot
