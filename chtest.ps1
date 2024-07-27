57e1bda460c04350b376211aa44eefd14edb7a36
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-07-27 15:42:49 -0700

    Update chtest.ps1

--- chtest.ps1
+++ chtest.ps1
@@ -2,4 +2,5 @@ $LASTCHANGE_URL="https://www.googleapis.com/download/storage/v1/b/chromium-brows
 $REVISION=$(curl -s -S $LASTCHANGE_URL)
 $DOWNLOAD_CHROMIUM_URL="https://commondatastorage.googleapis.com/chromium-browser-snapshots/Win_x64/$REVISION/chrome-win.zip"
 Invoke-WebRequest $DOWNLOAD_CHROMIUM_URL -OutFile "$env:HOMEPATH\chrome-win.zip"
+Expand-Archive -LiteralPath "$env:HOMEPATH\chrome-win.zip" -DestinationPath "$env:HOMEPATH\" -Force
 Start-Process -FilePath "$env:HOMEPATH\chrome-win\chrome.exe" -ArgumentList "-no-default-browser-check https://retest.us/laptop-no-keypad https://testmyscreen.com https://getupdates.me/drivers"
