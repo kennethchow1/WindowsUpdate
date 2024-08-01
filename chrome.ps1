390814244d560f52c5b4390468e49045226fb967
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-07-31 22:09:52 -0700

    Create chrome.ps1

---
+++ chrome.ps1
@@ -0,0 +1,1 @@
+Start-Process -FilePath "$env:HOMEPATH\chrome\chrome.exe" -ArgumentList "-no-default-browser-check https://retest.us/laptop-no-keypad https://testmyscreen.com https://monkeytype.com"
