da3a5d848765e629d7dd71339ebb84c8db3b6211
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-08-02 13:18:36 -0700

    Update chrome.ps1

--- chrome.ps1
+++ chrome.ps1
@@ -1,1 +1,1 @@
-Start-Process -FilePath "$env:HOMEPATH\chrome\chrome.exe" -ArgumentList "-no-default-browser-check https://retest.us/laptop-no-keypad https://testmyscreen.com https://typings.gg"
+Start-Process -FilePath "$env:HOMEPATH\chrome\chrome.exe" -ArgumentList "-no-default-browser-check https://retest.us/laptop-no-keypad https://testmyscreen.com https://monkeytype.com"
