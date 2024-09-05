ae9c750c0b0585047d767bb0125bfde7d050db75
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2024-09-05 14:57:29 -0700

    Update Part4.ps1

--- Part4.ps1
+++ Part4.ps1
@@ -12,7 +12,7 @@ $key=(Get-CimInstance -Class SoftwareLicensingService).OA3xOriginalProductKey
 iex "cscript /b C:\windows\system32\slmgr.vbs /upk"
 iex "cscript /b C:\windows\system32\slmgr.vbs /ipk $key"
 iex "cscript /b C:\windows\system32\slmgr.vbs /ato"
-echo "Checking if Windows is Activated... If it doesn't show as Licensed, you will have to manually activate Windows."
+echo "Verifying if Windows is Activated... If it doesn't show as Licensed, you will have to manually activate Windows."
 $ActivationStatus = Get-CimInstance SoftwareLicensingProduct -Filter "Name like 'Windows%'" | Where-Object { $_.PartialProductKey } | Select-Object LicenseStatus       
 
     $LicenseResult = switch($ActivationStatus.LicenseStatus){
