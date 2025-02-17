7ea7ee4c071ee734e8c6b66d54b11f41f52e5091
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2025-02-17 12:01:20 -0800

    Update hp.ps1

--- hp.ps1
+++ hp.ps1
@@ -1,3 +1,3 @@
-Install-Module -Name HPCMSL -Force -AcceptLicense
+Start-Process -FilePath "C:\Program Files\Powershell\7\pwsh.exe" -ArgumentList "-ExecutionPolicy Unrestricted -C irm https://raw.githubusercontent.com/ConfigJon/Firmware-Management/refs/heads/master/HP/Install-HPCMSL.ps1 | iex"
 Get-HPBIOSSetting -Name "Absolute Persistence Module Current State"
 Set-HPBIOSSettingValue -Name "Permanent Disable Absolute Persistence Module Set Once" -Value "Yes"
