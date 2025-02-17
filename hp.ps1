8b927084f3f2d20004c6a3819f59e3df5ad55c13
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2025-02-17 13:35:01 -0800

    Update hp.ps1

--- hp.ps1
+++ hp.ps1
@@ -1,5 +1,6 @@
 Invoke-WebRequest -Uri "https://hpia.hpcloud.hp.com/downloads/cmsl/hp-cmsl-1.8.1.exe" -OutFile "$env:HOMEPATH\hp-cmsl.exe"
 Start-Process -Wait -FilePath "$env:HOMEPATH\hp-cmsl.exe" -Argument "/VERYSILENT" -PassThru
+Set-ExecutionPolicy RemoteSigned
 Get-HPBIOSSetting -Name "Absolute Persistence Module Current State"
 Set-HPBIOSSettingValue -Name "Permanent Disable Absolute Persistence Module Set Once" -Value "Yes"
 echo "Rebooting to apply changes..."
