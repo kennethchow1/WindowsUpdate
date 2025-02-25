b8bb5541df7fe329de2c4a7e28d0deda1f0185a7
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2025-02-25 08:13:04 -0800

    Delete test21.ps1

--- test21.ps1
+++
@@ -1,6 +0,0 @@
-Install-Module PSWindowsUpdate -Confirm:$false -force
-Add-WUServiceManager -MicrosoftUpdate -Confirm:$false
-echo "Starting Updates"
-Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot
-Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot
-Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
