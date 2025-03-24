e90f0479b0a87760c0d86d6c62d2780e986734e0
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2025-03-24 11:44:25 -0700

    Update activate.ps1

--- activate.ps1
+++ activate.ps1
@@ -1,1 +1,1 @@
-Start-Process -FilePath "C:\Program Files\Powershell\7\pwsh.exe" -ArgumentList "-C irm https://get.activated.win | iex"
+Start-Process -FilePath "C:\Program Files\Powershell\7\pwsh.exe" -ArgumentList "-ExecutionPolicy unrestricted -C irm https://get.activated.win | iex"
