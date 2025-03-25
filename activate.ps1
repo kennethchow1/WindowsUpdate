3550ee1ecb9fec8f9454ef807da1080011b7f990
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2025-03-25 10:35:45 -0700

    Update activate.ps1

--- activate.ps1
+++ activate.ps1
@@ -1,1 +1,4 @@
-Start-Process -FilePath "C:\system32\WindowsPowershell\v1.0\powershell.exe" -ArgumentList "-ExecutionPolicy unrestricted -C irm https://get.activated.win | iex"
+$MASUrl = "https://github.com/massgravel/Microsoft-Activation-Scripts/archive/refs/heads/master.zip"
+$DownloadMAS = "$env:TEMP" + $(Split-Path -Path $Url -Leaf)
+Invoke-WebRequest -Uri $MASUrl -OutFile $DownloadMAS -TimeoutSec 30
+cmd.exe /c "$DownloadZipFile\MAS_AIO.cmd"
