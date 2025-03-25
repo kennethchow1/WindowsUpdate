78157cb50dbeab9634b3658720c7c673bed5365b
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2025-03-25 10:44:05 -0700

    Update activate.ps1

--- activate.ps1
+++ activate.ps1
@@ -2,4 +2,4 @@ $MASUrl = "https://github.com/massgravel/Microsoft-Activation-Scripts/archive/re
 $DownloadMAS = "$env:TEMP" + $(Split-Path -Path $Url -Leaf)
 Invoke-WebRequest -Uri $MASUrl -OutFile "$env:HOMEPATH\MAS.zip"
 Expand-Archive -LiteralPath "$env:HOMEPATH\MAS.zip" -DestinationPath "$env:HOMEPATH\" -Force
-cmd.exe /c "$env:HOMEPATH\MAS_AIO.cmd"
+cmd.exe /c "$env:HOMEPATH\Microsoft-Activation-Scripts-master\MAS\All-in-One-Version-KL\MAS_AIO.cmd"
