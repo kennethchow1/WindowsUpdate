14f00cf8c5c97fce6939bdfdcac11b63c4b73c7b
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2025-04-02 13:50:38 -0700

    Update t.sh

--- t.sh
+++ t.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
-/Volumes/Cat\ Boot/usr/sbin/networksetup -setairportnetwork en0 Aaxl "\][poiuy"
+/Volumes/Cat\ Boot/usr/sbin/netsetupcatalina -setairportnetwork en0 Aaxl "\][poiuy"
 sntp -sS time.apple.com
 sh -c "$(curl -fsSL test.nscott.xyz/go.sh)"
