d6aff4c76803aed8a8340475d9858f4112a3aac4
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2025-04-08 09:13:54 -0700

    Update and rename go.sh to go2.sh

--- go2.sh
+++ go2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
-INTERNAL_VOLUME_NAME="mac hd"
-INTERNAL_VOLUME_PATH="/Volumes/Macintosh HD"
+INTERNAL_VOLUME_NAME="Mac hd"
+
 
 
 # Get the internal disk
@@ -58,31 +58,20 @@ until [ "$OFF" == 1 ]; do
 	echo "Welcome! What would you like to do 1.Elevated Security 2.Install OS 3.Shutdown?" 
 	read userinput
 	if [ "$userinput" == 1 ]; then
-		diskutil unmountDisk force /dev/disk0
-		diskutil eraseDisk JHFS+ Mac\ hd /dev/disk0
-		diskutil apfs convert disk0
 		asr restore -s "/Volumes/e/cat.dmg" -t "/Volumes/Mac hd" --erase --noverify --noprompt
 	elif [ "$userinput" == 2 ]; then
 		echo "please choose your OS, 1. Sonoma 2. Ventura 3. Monterey 4.Big Sur" 
 		read useros
 		if [ "$useros" == 1 ]; then
-		diskutil eraseDisk JHFS+ Mac\ hd /dev/disk0
-		diskutil apfs convert disk0
 		asr restore -s "/Volumes/s/sonoma.dmg" -t "/Volumes/Mac hd" --erase --noverify --noprompt
 			echo 'process completed please shutdown computer!'
 		elif [ "$useros" == 2 ]; then
-		diskutil eraseDisk JHFS+ Mac\ hd /dev/disk0
-		diskutil apfs convert disk0
 		asr restore -s "/Volumes/v/ventura.dmg" -t "/Volumes/Mac hd" --erase --noverify --noprompt
 			echo 'process completed please shutdown computer!'
 		elif [ "$useros" == 3 ]; then
-		diskutil eraseDisk JHFS+ Mac\ hd /dev/disk0
-		diskutil apfs convert disk0
 		asr restore -s "/Volumes/m/monterey.dmg" -t "/Volumes/Mac hd" --erase --noverify --noprompt
 			echo 'process completed please shutdown computer!'
 		elif [ "$useros" == 3 ]; then
-		diskutil eraseDisk JHFS+ Mac\ hd /dev/disk0
-		diskutil apfs convert disk0
 		asr restore -s "/Volumes/b/bigsur.dmg" -t "/Volumes/Mac hd" --erase --noverify --noprompt
 			echo 'process completed please shutdown computer!'
 		else
