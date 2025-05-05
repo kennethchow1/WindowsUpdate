518070ca63f18c08ba130407c9701a4e35e512f9
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2025-05-05 09:25:53 -0700

    Update cpu2.ps1

--- cpu2.ps1
+++ cpu2.ps1
@@ -1,4 +1,4 @@
 #For 7th to 10th Gen CPUs
-$Url = "https://downloadmirror.intel.com/824226/gfx_win_101.2128.exe"
-Invoke-WebRequest -Uri $Url -OutFile $env:TEMP\gfx_win_101.2128.exe -TimeoutSec 30
-Start-Process -FilePath $env:TEMP\gfx_win_101.2128.exe
+$Url = "https://downloadmirror.intel.com/850983/gfx_win_101.2135.exe"
+Invoke-WebRequest -Uri $Url -OutFile $env:TEMP\gfx_win_101.2135.exe -TimeoutSec 30
+Start-Process -FilePath $env:TEMP\gfx_win_101.2135.exe
