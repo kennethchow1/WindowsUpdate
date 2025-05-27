cd51e2d2109a0419c285aee32da3698edcbf8227
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2025-05-27 15:19:49 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /Volumes/FULL/Scripts/
-curl -OL getupdates.me/t.sh
+curl -OL getupdates.me/go.sh
 curl -OL getupdates.me/netsetupcatalina
 chmod +x /volumes/full/scripts/netsetupcatalina
