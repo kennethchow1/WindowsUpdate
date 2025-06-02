a85ebb863eb7497588a4ec1b55b1df042d0c9fda
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2025-06-02 12:45:36 -0700

    Update Part2.ps1

--- Part2.ps1
+++ Part2.ps1
@@ -1,6 +1,11 @@
+$startTime = Get-Date
 Install-Module PSWindowsUpdate -Confirm:$false -force
 Add-WUServiceManager -MicrosoftUpdate -Confirm:$false
 echo "Starting Updates"
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
+$endTime = Get-Date
+$logFile = "$env:HOMEPATH\Documents\timestamp.txt"
+"Update Part 1 started at: $startTime" | Out-File -FilePath $logFile
+"Update Part 1 completed at: $endTime" | Out-File -FilePath $logFile -Append
