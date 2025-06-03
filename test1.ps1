3dc126e40f4c57bcf0814dff2c09460ce4aa5db3
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2025-06-03 11:45:40 -0700

    Update test1.ps1

--- test1.ps1
+++ test1.ps1
@@ -11,7 +11,6 @@ $logFile = "$env:HOMEPATH\Documents\timestamp.txt"
 "Update Part 1 completed at: $endTime" | Out-File -FilePath $logFile -Append
 $taskName = "RunMyShortcut"
 $shortcutPath = "$env:HOMEPATH\Desktop\Finalize.lnk"
-
 $action = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c start `"`" `"$shortcutPath`""
 $trigger = New-ScheduledTaskTrigger -AtLogOn
 Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -RunLevel Highest -User "SYSTEM"
