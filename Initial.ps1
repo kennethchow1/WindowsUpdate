6f7085b9a0d4f2b751528043f32800e359802273
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2025-06-12 11:10:05 -0700

    Update Initial.ps1

--- Initial.ps1
+++ Initial.ps1
@@ -1,4 +1,7 @@
-Start-Sleep 15
+Write-Log "Waiting for explorer.exe to start..."
+while (-not (Get-Process explorer -ErrorAction SilentlyContinue)) {
+    Start-Sleep -Seconds 15
+}
 $fullHomePath = Join-Path -Path $env:SystemDrive -ChildPath $env:HOMEPATH
 $logRoot = "$fullHomePath\WSUSLogs"
 $scriptPath = "$logRoot\WSUSUpdateMultiStage.ps1"
