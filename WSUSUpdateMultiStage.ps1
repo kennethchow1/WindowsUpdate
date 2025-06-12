44c006d86ae6114da6053e18257728d64bf180a2
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2025-06-12 10:20:09 -0700

    Update WSUSUpdateMultiStage.ps1

--- WSUSUpdateMultiStage.ps1
+++ WSUSUpdateMultiStage.ps1
@@ -132,7 +132,7 @@ function Schedule-NextRun {
     $action = New-ScheduledTaskAction -Execute $psPath -Argument "-ExecutionPolicy unrestricted -File `"$notscript`""
     $trigger = New-ScheduledTaskTrigger -AtLogOn
 
-    $user = "Administrator"
+    $user = "$env:COMPUTERNAME\Administrator"
     $logonType = "Interactive"
 
     # Create the principal with the right logon type and user
