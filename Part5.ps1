385ac6c5f19ed4a6660d8a29cdf982e8ed769c18
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2025-06-17 12:59:59 -0700

    Update Part5.ps1

--- Part5.ps1
+++ Part5.ps1
@@ -1,4 +1,4 @@
-$Url = "https://raw.githubusercontent.com/kennethchow1/WindowsUpdate/refs/heads/main/batteryinfoview.zip"
+$Url = "hhttps://files.getupdates.me/batteryinfoview.zip"
 $DownloadZipFile = "$env:TEMP" + $(Split-Path -Path $Url -Leaf)
 Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile -TimeoutSec 30
 Start-Process -FilePath $DownloadZipFile\BatteryInfoView.exe
