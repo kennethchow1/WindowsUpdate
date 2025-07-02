145283cd6e4193f1a0603171a61c61313efa9ded
kennethchow1 <103966331+kennethchow1@users.noreply.github.com>
2025-07-02 13:36:54 -0700

    Update cpu1.ps1

--- cpu1.ps1
+++ cpu1.ps1
@@ -1,4 +1,4 @@
 #For Intel 11th Gen and newer CPUs
-Invoke-WebRequest -Uri "https://downloadmirror.intel.com/853993/gfx_win_101.6790.exe" -OutFile $env:TEMP\gfx_win_101.6790.exe -TimeoutSec 30
+Invoke-WebRequest -Uri "https://downloadmirror.intel.com/858734/gfx_win_101.6913.exe" -OutFile $env:TEMP\gfx_win_101.6913.exe -TimeoutSec 30
 Start-Process -FilePath $env:TEMP\gfx_win_101.6790.exe
 
